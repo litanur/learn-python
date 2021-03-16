0e26cef5a71c19b63916b0658188ea8b70bdb2a7
Lita Nurlaelati <litanurlaelati@gmail.com>
2021-03-16 16:22:11 +0700

    Create fungsi.py
    Pengenalan fungsi

---
+++ fungsi.py
@@ -0,0 +1,9 @@
+def kali(angka1, angka2):
+    # Kalikan kedua parameter
+    hasil = angka1 * angka2
+    print('Dicetak dari dalam fungsi: {}'.format(hasil))
+    return hasil
+ 
+# Panggil fungsi kali
+keluaran = kali(10, 20);
+print('Dicetak sebagai kembalian: {}'.format(keluaran))
