4db51665ea6da0c8e59ede5bb31728cd6774485a
litanur <litanurlaelati@gmail.com>
2021-03-23 11:47:17 +0700

    Add syntax super() when override method

--- pewarisan.py
+++ pewarisan.py
@@ -17,16 +17,22 @@ class KalkulatorKali(Kalkulator):
         self.nilai = angka1 * angka2
         return self.nilai
 
-    """override method tambah_angka"""
+class KalkulatorTambah(Kalkulator):
+    """contoh mewarisi kelas kalkulator sederhana"""
+ 
     def tambah_angka(self, angka1, angka2):
-        self.nilai = angka1 + angka2
+        if angka1 + angka2 <= 9:  # fitur ini sudah oke di kelas dasar, gunakan yang ada saja
+            super().tambah_angka(angka1, angka2)  # panggil fungsi dari Kalkulator lalu isi nilai
+        else:  # ini adalah fitur baru yang ingin diperbaiki dari keterbatasan kelas dasar
+            self.nilai = angka1 + angka2
         return self.nilai
 
 kk = KalkulatorKali()
 a = kk.kali_angka(2, 3)  # sesuai dengan definisi class memiliki fitur kali_angka
 print(a)
- 
-b = kk.tambah_angka(5, 6)  # memiliki fitur tambah_angka karena mewarisi dari Kalkulator
+
+kt = KalkulatorTambah() 
+b = kt.tambah_angka(5, 6)  # memiliki fitur tambah_angka karena mewarisi dari Kalkulator
 print(b)
 
 # run on https://ideone.com/TKZcmX 
